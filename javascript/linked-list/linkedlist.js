328455963bd09e455e0c697f01990db48a96d04f
Neveen97 <neveenaburomman1@gmail.com>
2022-03-07 23:36:53 +0300

    commit on linked-list

--- javascript/linked-list/linkedlist.js
+++ javascript/linked-list/linkedlist.js
@@ -16,14 +16,42 @@ class LinkedList {
             this.head = node;
         }
     }
-
+    append(value) {
+        const node = new Node(value);
+        if(!this.head) { 
+            this.head = node;
+        } else { 
+            let currentNode = this.head;
       
+            while (currentNode.next) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = node;
+        }
+      }
 
+      includes(value) {
+        
+        let  current = this.head; // Initialize current
 
+        while (current != null) {
 
+            if (current.data == value)
+
+                return true; // data found
+            current = current.next;
+        }
+        return false; // data not found
     
   }
+   
+  
 
+
+
+
+
+}
 
 
 module.exports = LinkedList;
