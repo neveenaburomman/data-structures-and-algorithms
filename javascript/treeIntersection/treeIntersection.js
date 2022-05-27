43a2f1afa2de123ff990e56c8e544dcdfb9a86e3
Neveen97 <neveenaburomman1@gmail.com>
2022-05-28 01:18:28 +0300

    ch32 done

--- javascript/treeIntersection/treeIntersection.js
+++ javascript/treeIntersection/treeIntersection.js
@@ -1,35 +1,46 @@
 'use strict';
 
-const  HashTable  = require('../hashTable/hashTable')
+const HashTable = require('../hashTable/hashTable')
 
 function treeIntersection(tree1, tree2) {
-    const firstTree = tree1.preOrder();
-    const secondTree = tree2.preOrder();
-
-    let trees = [firstTree, secondTree];
-    let hashtable = new HashTable();
-    let intersectionHash = new HashTable();
-
     let intersectionArr = [];
-    for (let j = 0; j < 2; j++) {
-        for (let i = 0; i < trees[j].length; i++) {
-            let element = trees[j][i];
-            if (!hashtable.contain(element)) {
-                hashtable.set(element, element)
 
-            } else if (!intersectionHash.contain(element)) {
-                intersectionHash.set(element, element);
-                intersectionArr.push(element);
-            }
-        }
-    }
-    if (intersectionArr.length=== 0) {
+ const traverse = node =>{
+   intersectionArr.push(node.value);
+   if (node.left) traverse(node.left);
+   if (node.right) traverse(node.right);
+ };
+ traverse(tree1.root);
+ traverse(tree2.root);
 
-        return "no intersections "
-    }
-    return intersectionArr;
+ let hashTable1 = new HashTable(100);
+
+ for (let i = 0; i < intersectionArr.length; i++) {
+if(hashTable1.table[i]!== undefined){
+ }
+   hashTable1.set(`${intersectionArr[i]}`,intersectionArr[i]);
+   
+ }
+ 
+ let arr = [];
+
+ for (let i = 0; i < hashTable1.table.length; i++) {
+
+   if ( hashTable1.table[i] && hashTable1.table[i].head.next){
+
+     arr.push( hashTable1.table[i].head.value.value);
+
+   }
+ }
+if(arr.length==0){
+ return "no intersection"
+}
+else{
+ return arr;
+}
 }
 
+       
 
 
 module.exports = treeIntersection;
--- javascript/treeIntersection/treeIntersection.md
+++ javascript/treeIntersection/treeIntersection.md
@@ -7,8 +7,8 @@ Write a function called tree_intersection that takes two binary trees as paramet
 
 ## Approach & Efficiency
 
-- Time complexity is o(n^2)
-- space complexity is o(n)
+- Time complexity is o(n^2) we will loop through two binary tree.
+- space complexity is o(n)because it will depneds on the size of the array.
 
 ## Solution
 ```ruby
